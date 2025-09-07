a5ee2b4b6f5cfa0940ff8f598a8c390512d87807
Makvana Jayraj <makvana.jayraj76537@gmail.com>
2025-09-07 09:50:12 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
 
 export const connrctDB = async () => {
-    await mongoose.connect('mongodb+srv://jayrajmakvana:9316701873@cluster0.cqoyyqr.mongodb.net/food-del').then(() => console.log("DB Connected"));
+    await mongoose.connect('Enter Yuor database Url').then(() => console.log("DB Connected"));
 }
